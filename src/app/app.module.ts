40cc7112e5eca2e715a61cb8470fe47716f131f4
vishwas.prabhu <vishwas.prabhu@robosoftin.com>
2021-07-01 13:38:10 +0530

    test(printer.service): Added missing tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
@@ -18,7 +18,7 @@ import { MaterialModule } from './shared/material.module'
     HttpClientModule,
     MaterialModule,
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/services/printer.service.spec.ts
+++ src/app/core/services/printer.service.spec.ts
@@ -3,8 +3,31 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
+import { PrinterListResponse } from 'src/app/shared/interfaces/printer-data'
 
 import { PrinterService } from './printer.service'
+
+const printerData = {
+  printerList: [
+    {
+      id: 'HPEPMC10501',
+      name: '255.453.126.56',
+      status: 'true',
+    },
+    {
+      id: 'HPEPMC10502',
+      name: '26.453.126.56',
+      status: 'true',
+    },
+    {
+      id: 'HPEPMC10503',
+      name: '87.453.126.56',
+      status: 'true',
+    },
+  ],
+  totalPages: 3,
+  pageNumber: 1,
+} as PrinterListResponse
 
 describe('PrinterService', () => {
   let service: PrinterService
@@ -24,5 +47,30 @@ describe('PrinterService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should return data of type PrinterListResponse from getPrintersData function', () => {
+    service.getPrintersData().subscribe(data => {
+      expect(data).toEqual(printerData)
+      expect(data.printerList[0].id).toEqual('HPEPMC10501')
+      expect(data.printerList[1].name).toEqual('26.453.126.56')
+    })
+    const req = httpTestingController.expectOne(
+      'http://localhost:3001/printerList'
+    )
+    req.flush(printerData)
+  })
+
+  it('should return data of type PrinterListResponse from getPrintersDataByPage function', () => {
+    const pageNo = 1
+    service.getPrintersDataByPage(pageNo).subscribe(data => {
+      expect(data).toEqual(printerData)
+      expect(data.printerList[0].id).toEqual('HPEPMC10501')
+      expect(data.printerList[1].name).toEqual('26.453.126.56')
+    })
+    const req = httpTestingController.expectOne(
+      `http://localhost:3001/printerList/${pageNo}`
+    )
+    req.flush(printerData)
   })
 })
