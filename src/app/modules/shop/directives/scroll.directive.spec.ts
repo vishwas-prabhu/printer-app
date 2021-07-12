56fc45735b45070fea0326cb9d8224b1c96a8dc9
vishwas.prabhu <vishwas.prabhu@robosoftin.com>
2021-07-12 15:39:51 +0530

    test(directive): Added tests for diectives

--- src/app/modules/shop/directives/scroll.directive.spec.ts
+++ src/app/modules/shop/directives/scroll.directive.spec.ts
@@ -1,8 +1,32 @@
-import { ScrollDirective } from './scroll.directive';
+import { ScrollDirective } from './scroll.directive'
+
+const data = {
+  target: {
+    scrollHeight: 820,
+    clientHeight: 690,
+    scrollTop: 110,
+  },
+}
 
 describe('ScrollDirective', () => {
   it('should create an instance', () => {
-    const directive = new ScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+    const directive = new ScrollDirective()
+    expect(directive).toBeTruthy()
+  })
+
+  it('should emit loadNextPage event on scroll down', () => {
+    const directive = new ScrollDirective()
+    directive.previousOffsetTop = 100
+    spyOn(directive.loadNextPage, 'emit')
+    directive.onScroll(data)
+    expect(directive.loadNextPage.emit).toHaveBeenCalled()
+  })
+
+  it('should not emit loadNextPage event on scroll up', () => {
+    const directive = new ScrollDirective()
+    directive.previousOffsetTop = 130
+    spyOn(directive.loadNextPage, 'emit')
+    directive.onScroll(data)
+    expect(directive.loadNextPage.emit).not.toHaveBeenCalled()
+  })
+})
